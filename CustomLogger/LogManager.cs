b2d7e80243f4c946b8a4252f3909e89b2947e200
Roman Moravskyi <moravsky12@gmail.com>
2019-10-22 14:36:44 +0300

    implemeted LogManger

--- CustomLogger/LogManager.cs
+++ CustomLogger/LogManager.cs
@@ -4,18 +4,48 @@ namespace CustomLogger
 {
     public static class LogManager
     {
-        public static LoggingConfiguration Configuration { get; set; }
+        private static LoggingConfiguration _config;
+
+        static LogManager()
+        {
+            LogFactory = new LogFactory();
+        }
+            
+        public static LoggingConfiguration Configuration
+        {
+            get => _config;
+            set
+            {
+                _config = value;
+                LogFactory.LoggingConfiguration = _config;
+            }
+        }
 
         public static LogFactory LogFactory { get; }
 
         public static Logger GetLogger(string name)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("name cannot be null, empty or whitespace", nameof(name));
+            }
+
+            return LogFactory.GetLogger(name);
         }
 
+        public static Logger GetLoggerForType(string name, Type type)
+        {
+            return GetLogger($"{name}{type?.FullName}");
+        }
+
+        /// <summary>
+        /// Call this method to shut down all loggers
+        /// </summary>
         public static void Shutdown()
         {
-            throw new NotImplementedException();
+            Configuration.Dispose();
+            Configuration = new LoggingConfiguration();
+            LogFactory.LoggingConfiguration = new LoggingConfiguration();
         }
 
     }
