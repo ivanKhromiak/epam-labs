73a5486a2b4ca3601a2dee5d1029eeee89cc8107
Roman Moravskyi <moravsky12@gmail.com>
2019-10-22 14:37:02 +0300

    implemented LogLevel Writes

--- CustomLogger/Logger.cs
+++ CustomLogger/Logger.cs
@@ -1,69 +1,59 @@
-﻿using System;
-using CustomLogger.Interfaces;
-
-namespace CustomLogger
+﻿namespace CustomLogger
 {
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// Logs messages with targets logging config
+    /// </summary>
     public class Logger : ILogger
     {
-        private ITargetWriter[] _writers;
-
-        protected internal Logger()
+        internal Logger(string name, LogFactory logFactory, LoggingConfiguration configuration)
         {
-
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            LogFactory = logFactory ?? throw new ArgumentNullException(nameof(logFactory));
+            Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
         public string Name { get; }
 
         public LogFactory LogFactory { get; }
 
-        public void Debug(string message)
-        {
-            throw new NotImplementedException();
-        }
+        internal LoggingConfiguration Configuration { get; set; }
 
         public void Debug(string message, Exception exception)
         {
-            throw new NotImplementedException();
-        }
-
-        public void Error(string message)
-        {
-            throw new NotImplementedException();
+            Log($"{GetTimeStamp()}: {message}\n" +
+                $"Exception: {GetInfoFromException(exception, LogLevel.Debug)}");
         }
 
         public void Error(string message, Exception exception)
         {
-            throw new NotImplementedException();
-        }
-
-        public void Fatal(string message)
-        {
-            throw new NotImplementedException();
+            Log($"{GetTimeStamp()}: {message}\n" +
+                $"Exception: {GetInfoFromException(exception, LogLevel.Error)}");
         }
 
         public void Fatal(string message, Exception exception)
         {
-            throw new NotImplementedException();
-        }
-
-        public void Info(string message)
-        {
-            throw new NotImplementedException();
+            Log($"{GetTimeStamp()}: {message}\n" +
+                $"Exception: {GetInfoFromException(exception, LogLevel.Fatal)}");
         }
 
         public void Info(string message, Exception exception)
         {
-            throw new NotImplementedException();
+            Log($"{GetTimeStamp()}: {message}\n" +
+                $"Exception: {GetInfoFromException(exception, LogLevel.Info)}");
         }
 
         public void LogMessage(string message, LogLevel level)
         {
-            throw new NotImplementedException();
+            Log($"{GetTimeStamp()}: {message}");
         }
 
-        public void Trace(string message)
+        public void LogMessage(string message, LogLevel level, Exception exception)
         {
-            throw new NotImplementedException();
+            Log($"{GetTimeStamp()}: {message}\n" +
+                $"Exception: {GetInfoFromException(exception, level)}");
         }
 
         public void Trace(string message, Exception exception)
@@ -71,14 +61,95 @@ namespace CustomLogger
             throw new NotImplementedException();
         }
 
-        public void Warn(string message)
+        public void Warn(string message, Exception exception)
+        {
+            Log($"{GetTimeStamp()}: {message}\n" +
+                $"Exception: {GetInfoFromException(exception, LogLevel.Warn)}");
+        }
+
+        private void Log(string message)
+        {
+            WriteToAllTargets($"{message}\n");
+        }
+
+        private void LogAsync(string message)
         {
             throw new NotImplementedException();
         }
 
-        public void Warn(string message, Exception exception)
+        private void WriteToAllTargets(string message)
+        {
+            var targetWriters = Configuration.GetTargetWriters();
+
+            foreach(var writer in targetWriters)
+            {
+                writer.WriteMessage(message);
+            }
+        }
+
+        private void WriteToAllTargetsAsync(string message)
         {
             throw new NotImplementedException();
+        }
+
+        private string GetInfoFromException(Exception e, LogLevel level)
+        {
+            var strBuilder = new StringBuilder();
+
+            if(level <= LogLevel.Trace)
+            {
+                AppendTraceInfo(e, strBuilder);
+            }
+            if(level <= LogLevel.Debug)
+            {
+                AppendDebugInfo(e, strBuilder);
+            }
+            if (level <= LogLevel.Warn)
+            {
+                strBuilder.Append($"Exception message: {e?.Message};");
+            }
+            
+            return strBuilder.ToString();
+        }
+
+        private static void AppendDebugInfo(Exception e, StringBuilder strBuilder)
+        {
+            if (e is null)
+            {
+                return;
+            }
+
+            if (strBuilder is null)
+            {
+                throw new ArgumentNullException(nameof(strBuilder));
+            }
+
+            strBuilder.Append($"Module Name: {e.TargetSite.Module.FullyQualifiedName}");
+            strBuilder.Append($"TargetSite Name: {e.TargetSite.Name}");
+            strBuilder.Append($"Source: {e.Source}");
+            strBuilder.Append($"Help link: {e.HelpLink}");
+        }
+
+        private void AppendTraceInfo(Exception e, StringBuilder strBuilder)
+        {
+            if(e == null)
+            {
+                return;
+            }
+
+            if (strBuilder is null)
+            {
+                throw new ArgumentNullException(nameof(strBuilder));
+            }
+
+            strBuilder.Append($"Stack trace: {e.StackTrace}; ");
+            strBuilder.Append("Inner exception info: ");
+            strBuilder.Append(GetInfoFromException(e?.InnerException, LogLevel.Trace));
+        }
+
+        private static string GetTimeStamp()
+        {
+            return DateTime.Now.ToString();
         }
     }
 }
