5b9e5074b566054c2f6b942f12adcff3bad276d0
Roman Moravskyi <moravsky12@gmail.com>
2019-10-24 20:32:12 +0300

    optimized cleanup and adding targets

--- CustomLogger/LoggingConfiguration.cs
+++ CustomLogger/LoggingConfiguration.cs
@@ -2,11 +2,13 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using CustomLogger.Internal;
 
     public class LoggingConfiguration : IDisposable
     {
         private readonly List<Target> _targets;
+        private readonly List<Target> _targetsCache;
         private List<ITargetWriter> _writers;
 
         internal event Action LoggingConfigChanged;
@@ -14,65 +16,108 @@
         public LoggingConfiguration()
         {
             _targets = new List<Target>();
+            _targetsCache = new List<Target>();
             _writers = null;
         }
 
         public LoggingConfiguration AddFileTarget(string path)
         {
             // target already exists
-            foreach(var target in _targets)
+            if (!IsAlreadyAddedFileTarget(path))
             {
-                if(target is FileTarget fileTarget && fileTarget.Path == path)
-                {
-                    return this;
-                }
+                _targets.Add(new FileTarget(path));
             }
-
-            _targets.Add(new FileTarget(path));
-            ReconfigTargetWriters();
 
             return this;
         }
 
         public LoggingConfiguration AddConsoleTarget()
         {
-            _targets.Add(new ConsoleTarget());
-            ReconfigTargetWriters();
+            // target already exists
+            if (!IsAlreadyAddedTarget<ConsoleTarget>())
+            {
+                _targets.Add(new ConsoleTarget());
+            }
 
             return this;
+        }
+
+        private bool IsAlreadyAddedTarget<T>()
+            where T : Target
+        {
+            bool isPresentTarget = false;
+
+            if (_targets.Count > 0)
+            {
+                isPresentTarget = _targets.Any(t => t is T);
+            }
+            if (_targetsCache.Count > 0)
+            {
+                isPresentTarget = _targetsCache.Any(t => t is T);
+            }
+
+            return isPresentTarget;
+        }
+
+        private bool IsAlreadyAddedFileTarget(string path)
+        {
+            bool Predicate(Target t)
+            {
+                return t is FileTarget tar && tar.Path == path;
+            }
+
+            bool isPresentTarget = false;
+
+            if (_targets.Count > 0)
+            {
+                isPresentTarget = _targets.Any(t => Predicate(t));
+            }
+            if (_targetsCache.Count > 0)
+            {
+                isPresentTarget = _targetsCache.Any(t => Predicate(t));
+            }
+
+            return isPresentTarget;
         }
 
         public void Reset()
         {
             _targets.Clear();
-            ReconfigTargetWriters();
+            _targetsCache.Clear();
+            ClearOldWriters();
         }
 
         internal IEnumerable<ITargetWriter> GetTargetWriters()
         {
-            if (_writers == null)
+            if (_writers == null || !DoAllTargetsHaveWriters())
             {
                 ReconfigTargetWriters();
             }
 
+            _targetsCache.AddRange(_targets);
+            _targets.Clear();
+
             return _writers;
+        }
+
+        private bool DoAllTargetsHaveWriters()
+        {
+            return _targets.Count + _targetsCache.Count == _writers.Count;
         }
 
         private void ReconfigTargetWriters()
         {
-            ClearOldWriters();
-            SetNewWriters();
+            if (_writers == null)
+            {
+                _writers = new List<ITargetWriter>();
+            }
 
-            LoggingConfigChanged?.Invoke();
-        }
-
-        private void SetNewWriters()
-        {
-            _writers = new List<ITargetWriter>();
             foreach (var target in _targets)
             {
                 _writers.Add(target.GetTargetWriter());
             }
+
+            LoggingConfigChanged?.Invoke();
         }
 
         private void ClearOldWriters()
