9e00039a553f80d4b263250deb3cf063f2253b48
Roman Moravskyi <moravsky12@gmail.com>
2019-10-29 12:40:00 +0200

    initial import

---
+++ Lab7/DirectoryComparer.cs
@@ -0,0 +1,77 @@
+﻿// <copyright file="DirectoryComparer.cs" company="Roman Moravskyi">
+// Copyright (c) Roman Moravskyi. All rights reserved.
+// </copyright>
+
+namespace Epam.HomeWork.Lab7
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    /// <summary>
+    /// Static class for comparing two directories
+    /// </summary>
+    public static class DirectoryComparer
+    {
+        /// <summary>
+        /// Returns filenames that have same name and exist in both directories
+        /// </summary>
+        /// <param name="firstDirectory">First directory info</param>
+        /// <param name="secondDirectory">Second directory info</param>
+        /// <returns>List of strings</returns>
+        public static IEnumerable<string> GetDuplicateFiles(DirectoryInfo firstDirectory, DirectoryInfo secondDirectory)
+        {
+            if (firstDirectory == null)
+            {
+                throw new System.ArgumentNullException(nameof(firstDirectory));
+            }
+
+            if (secondDirectory == null)
+            {
+                throw new System.ArgumentNullException(nameof(secondDirectory));
+            }
+
+            var firstDirFilenames = firstDirectory
+                .EnumerateFiles("*", SearchOption.AllDirectories)
+                .Select(fi => fi.Name);
+
+            var secondDirFilenames = secondDirectory
+                .EnumerateFiles("*", SearchOption.AllDirectories)
+                .Select(fi => fi.Name);
+
+            return firstDirFilenames.Intersect(secondDirFilenames);
+        }
+
+        /// <summary>
+        /// Returns filenames that are unique for two directories
+        /// </summary>
+        /// <param name="firstDirectory"></param>
+        /// <param name="secondDirectory"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> GetUniqueFiles(DirectoryInfo firstDirectory, DirectoryInfo secondDirectory)
+        {
+            if (firstDirectory == null)
+            {
+                throw new System.ArgumentNullException(nameof(firstDirectory));
+            }
+
+            if (secondDirectory == null)
+            {
+                throw new System.ArgumentNullException(nameof(secondDirectory));
+            }
+
+            var firstDirFilenames = firstDirectory
+                .EnumerateFiles("*", SearchOption.AllDirectories)
+                .Select(fi => fi.Name);
+
+            var secondDirFilenames = secondDirectory
+                .EnumerateFiles("*", SearchOption.AllDirectories)
+                .Select(fi => fi.Name);
+
+            return firstDirFilenames
+                .Except(secondDirFilenames)
+                .Union(secondDirFilenames
+                    .Except(firstDirFilenames));
+        }
+    }
+}
---
+++ Lab7Runner/Lab7Runner.cs
@@ -0,0 +1,88 @@
+﻿namespace Epam.HomeWork.Lab7Runner
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using Epam.HomeWork.Common;
+    using Epam.HomeWork.Lab7;
+    using Microsoft.Extensions.Configuration;
+
+    public class Lab7Runner : IConsoleLabRunner
+    {
+        private readonly IConfigurationRoot configuration;
+
+        public Lab7Runner()
+        {
+            Errors = new List<string>();
+            Success = false;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            configuration = builder.Build();
+        }
+
+        public string Description => "Lab7: Directory Comparer";
+
+        public IList<string> Errors { get; }
+
+        public bool Success { get; private set; }
+
+        public void RunConsoleLab()
+        {
+            Success = true;
+            
+            DirectoryInfo firstDir = new DirectoryInfo(configuration["FirstDirectoryName"]);
+            DirectoryInfo secondDir = new DirectoryInfo(configuration["SecondDirectoryName"]);
+
+            RunTask1(firstDir, secondDir);
+            RunTask2(firstDir, secondDir);
+        }
+
+        private void RunTask1(DirectoryInfo firstDir, DirectoryInfo secondDir)
+        {
+            ConsoleHelper.WriteHeaderMessage(
+                "Task 1: Info about duplicate diles in two directories...\n",
+                ConsoleColor.Yellow,
+                ConsoleColor.Black);
+
+            var duplicateFiles = DirectoryComparer.GetDuplicateFiles(firstDir, secondDir);
+
+            if(configuration["TargetOutput"] == "File")
+            {
+
+            }
+            else if(configuration["TargetOutput"] == "Console")
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+        private void RunTask2(DirectoryInfo firstDir, DirectoryInfo secondDir)
+        {
+            ConsoleHelper.WriteHeaderMessage(
+                "Task 2: Info about unique diles in two directories...\n",
+                ConsoleColor.Yellow,
+                ConsoleColor.Black);
+
+            var uniqueFiles = DirectoryComparer.GetUniqueFiles(firstDir, secondDir);
+
+            if (configuration["TargetOutput"] == "File")
+            {
+
+            }
+            else if (configuration["TargetOutput"] == "Console")
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+        
+    }
+}
