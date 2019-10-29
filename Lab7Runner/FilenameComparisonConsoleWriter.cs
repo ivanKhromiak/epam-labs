80918189e7ed98f6ce398a8ab15a0c4eb2904975
Roman Moravskyi <moravsky12@gmail.com>
2019-10-29 16:38:45 +0200

    added console writer

---
+++ Lab7Runner/FilenameComparisonConsoleWriter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Epam.HomeWork.Lab7Runner
+{
+    public class FilenameComparisonConsoleWriter : IFilenameComparisonWriter
+    {
+        public void WriteFilenameData(string headerMessage, IEnumerable<string> filenames, string filepath)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
