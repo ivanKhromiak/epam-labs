f5b3f1fd33e9e1f496b132f9acc49e93c5048422
Roman Moravskyi <moravsky12@gmail.com>
2019-10-29 11:13:08 +0200

    initial import

---
+++ Lab6/Common/PointF.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="PointF.cs" company="Roman Moravskyi">
+// Copyright (c) Roman Moravskyi. All rights reserved.
+// </copyright>
+
+namespace Epam.HomeWork.Lab6.Common
+{
+    using System;
+
+    /// <summary>
+    /// Defines a float point in coordinates
+    /// </summary>
+    public class PointF : ICloneable
+    {
+        /// <summary>
+        /// Initializes static members of the <see cref="PointF" /> class.
+        /// </summary>
+        static PointF()
+        {
+            Origin = new PointF(0.0f, 0.0f);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PointF" /> class.
+        /// </summary>
+        public PointF()
+            : this(0.0f, 0.0f)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PointF" /> class.
+        /// </summary>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y coordinate</param>
+        public PointF(float x, float y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        
+        /// <summary>
+        /// Gets origin of coordinates
+        /// </summary>
+        public static PointF Origin { get; }
+
+        /// <summary>
+        /// Gets or sets X position
+        /// </summary>
+        public float X { get; set; }
+
+        /// <summary>
+        /// Gets or sets Y position
+        /// </summary>
+        public float Y { get; set; }
+
+        /// <summary>
+        /// Clones an instance of <see cref="PointF" />
+        /// </summary>
+        /// <returns><see cref="PointF" /></returns>
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
---
+++ Lab6/Task1/Rectangle.cs
@@ -0,0 +1,219 @@
+﻿// <copyright file="Rectangle.cs" company="Roman Moravskyi">
+// Copyright (c) Roman Moravskyi. All rights reserved.
+// </copyright>
+
+namespace Epam.HomeWork.Lab6.Task1
+{
+    using System;
+    using Epam.HomeWork.Lab6.Common;
+
+    /// <summary>
+    /// Rectangle with coordinates, width and height
+    /// </summary>
+    public class Rectangle
+    {
+        /// <summary>
+        /// Backfield for <see cref="Width" />
+        /// </summary>
+        private float width;
+
+        /// <summary>
+        /// Backfield for <see cref="Height" />
+        /// </summary>
+        private float height;
+
+        /// <summary>
+        /// Backfield for <see cref="BottomLeft" />
+        /// </summary>
+        private PointF bottomLeft;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Rectangle" /> class.
+        /// </summary>
+        public Rectangle()
+            : this(0.0f, 0.0f, PointF.Origin)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Rectangle" /> class.
+        /// </summary>
+        /// <param name="width">Width of rectangle</param>
+        /// <param name="height">Height of rectangle</param>
+        /// <param name="bottomLeft">BottomLeft <see cref="PointF" /> of rectangle</param>
+        public Rectangle(float width, float height, PointF bottomLeft)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.BottomLeft = bottomLeft.Clone() as PointF;
+
+            this.BottomRight = new PointF(
+                this.BottomLeft.X + this.Width, 
+                this.BottomLeft.Y);
+
+            this.TopLeft = new PointF(
+                this.BottomLeft.X, 
+                this.BottomLeft.Y + this.Height);
+
+            this.TopRight = new PointF(
+                this.BottomLeft.X + this.Width, 
+                this.BottomLeft.Y + this.Height);
+        }
+
+        /// <summary>
+        /// Gets or sets width of current rectangle.
+        /// </summary>
+        public float Width
+        {
+            get => this.width;
+            set
+            {
+                if (value < 0.0f)
+                {
+                    throw new ArgumentException("Width cannot be less than zero.");
+                }
+
+                this.width = value;
+                this.RecalculatePoints();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets height of current rectangle.
+        /// </summary>
+        public float Height
+        {
+            get => this.height;
+            set
+            {
+                if (value < 0.0f)
+                {
+                    throw new ArgumentException("Width cannot be less than zero.");
+                }
+
+                this.height = value;
+                this.RecalculatePoints();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets <see cref="BottomLeft" /> of current rectangle.
+        /// </summary>
+        public PointF BottomLeft
+        {
+            get => this.bottomLeft;
+            set
+            {
+                this.bottomLeft = value;
+                this.RecalculatePoints();
+            }
+        }
+
+        /// <summary>
+        /// Gets <see cref="BottomRight" /> of current rectangle.
+        /// </summary>
+        public PointF BottomRight { get; private set; }
+
+        /// <summary>
+        /// Gets <see cref="TopLeft" /> of current rectangle.
+        /// </summary>
+        public PointF TopLeft { get; private set; }
+
+        /// <summary>
+        /// Gets <see cref="TopRight" /> of current rectangle.
+        /// </summary>
+        public PointF TopRight { get; private set; }
+
+        /// <summary>
+        /// Moves current rectangle to a new BottomLeft <see cref="PointF" /> .
+        /// </summary>
+        /// <param name="newBottomLeft">A new BottomLeft <see cref="PointF" /></param>
+        /// <returns><see cref="Rectangle" /> </returns>
+        public Rectangle MoveTo(PointF newBottomLeft)
+        {
+            this.BottomLeft.X = newBottomLeft.X;
+            this.BottomLeft.Y = newBottomLeft.Y;
+            this.RecalculatePoints();
+
+            return this;
+        }
+
+        /// <summary>
+        /// Scales current rectangle and returns it.
+        /// </summary>
+        /// <param name="cfX">Scaling coefficient for X</param>
+        /// <param name="cfY">Scaling coefficient for Y</param>
+        /// <returns><see cref="Rectangle" /></returns>
+        public Rectangle Scale(float cfX, float cfY)
+        {
+            this.Width *= cfX;
+            this.Height *= cfY;
+            this.RecalculatePoints();
+
+            return this;
+        }
+
+        /// <summary>
+        /// Scales current rectangle and returns it.
+        /// </summary>
+        /// <param name="cf">Scaling coefficient for Y and X</param>
+        /// <returns><see cref="Rectangle" /></returns>
+        public Rectangle Scale(float cf)
+        {
+            return this.Scale(cf, cf);
+        }
+
+        /// <summary>
+        /// Scales current rectangle and returns it.
+        /// </summary>
+        /// <param name="cf">Scaling coefficient for X</param>
+        /// <returns><see cref="Rectangle" /></returns>
+        public Rectangle ScaleX(float cf)
+        {
+            return this.Scale(cf, 1.0f);
+        }
+
+        /// <summary>
+        /// Scales current rectangle and returns it.
+        /// </summary>
+        /// <param name="cf">Scaling coefficient for Y</param>
+        /// <returns><see cref="Rectangle" /></returns>
+        public Rectangle ScaleY(float cf)
+        {
+            return this.Scale(1.0f, cf);
+        }
+
+        /// <summary>
+        /// Returns two rectangle intersect.
+        /// </summary>
+        /// <param name="other">Rectangle to intersect with</param>
+        /// <returns><see cref="Rectangle" /></returns>
+        public Rectangle Intersect(Rectangle other)
+        {
+            float leftX = MathF.Max(this.BottomLeft.X, other.BottomLeft.X);
+            float rightX = MathF.Min(this.BottomRight.X, other.BottomRight.X);
+            float bottomY = MathF.Max(this.BottomLeft.Y, other.BottomLeft.Y);
+            float topY = MathF.Min(this.TopLeft.Y, other.TopLeft.Y);
+
+            return new Rectangle(
+                rightX - leftX, 
+                topY - bottomY, 
+                new PointF(leftX, bottomY));
+        }
+
+        /// <summary>
+        /// Recalculates points of rectangle after BottomLeft or Height/Width changes.
+        /// </summary>
+        private void RecalculatePoints()
+        {
+            this.BottomRight.Y = this.BottomLeft.Y;
+            this.BottomRight.X = this.BottomLeft.X + this.Width;
+
+            this.TopLeft.X = this.BottomLeft.X;
+            this.TopLeft.Y = this.BottomLeft.Y + this.Height;
+
+            this.TopRight.X = this.BottomRight.X;
+            this.TopRight.Y = this.TopLeft.Y;
+        }
+    }
+}
