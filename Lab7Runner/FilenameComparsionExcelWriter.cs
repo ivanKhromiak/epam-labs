6f3fb83252d12ba2c8e5af4c43a74400ece3a170
Roman Moravskyi <moravsky12@gmail.com>
2019-10-29 16:36:16 +0200

    refactoring

--- Lab7Runner/FilenameComparsionExcelWriter.cs
+++ Lab7Runner/FilenameComparsionExcelWriter.cs
@@ -6,6 +6,13 @@ namespace Epam.HomeWork.Lab7Runner
 {
     public class FilenameComparsionExcelWriter : IFilenameComparisonWriter
     {
+        public FilenameComparsionExcelWriter(string path)
+        {
+            this.Path = path;
+        }
+
+        public string Path { get; }
+
         public void WriteFilenameData(string headerMessage, IEnumerable<string> filenames, string filepath)
         {
             Application excelApp;
@@ -25,31 +32,11 @@ namespace Epam.HomeWork.Lab7Runner
                 //Get a new workbook.
                 excelWorkBook = excelApp.Workbooks.Add("");
                 excelSheet = (_Worksheet)excelWorkBook.ActiveSheet;
-
-                //Add table headers going cell by cell.
-                excelSheet.Cells[1, 1] = "First Name";
-                excelSheet.Cells[1, 2] = "Last Name";
-                excelSheet.Cells[1, 3] = "Full Name";
-                excelSheet.Cells[1, 4] = "Salary";
-
-                //Format A1:D1 as bold, vertical alignment = center.
-                excelSheet.get_Range("A1", "D1").Font.Bold = true;
-                excelSheet.get_Range("A1", "D1").VerticalAlignment =
-                    XlVAlign.xlVAlignCenter;
-
-                // Create an array to multiple values at once.
-                string[,] saNames = new string[5, 2];
-
-                saNames[0, 0] = "John";
-                saNames[0, 1] = "Smith";
-                saNames[1, 0] = "Tom";
-
-                saNames[4, 1] = "Johnson";
                 
 
                 excelApp.Visible = false;
                 excelApp.UserControl = false;
-                excelWorkBook.SaveAs("c:\\test\\test505.xls", XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+                excelWorkBook.SaveAs(Path, XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
                     false, false, XlSaveAsAccessMode.xlNoChange,
                     Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
 
