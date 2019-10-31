32880d3518b7eaaf2c42c1d51b03ce49c4cc6e1c
Roman Moravskyi <moravsky12@gmail.com>
2019-10-31 16:08:15 +0200

    added column reader and StatisticInfo class

---
+++ HomeWork.LabRunners/Common/StatisticInfo.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="StatisticInfo.cs" company="Roman Moravskyi">
+// Copyright (c) Roman Moravskyi. All rights reserved.
+// </copyright>
+
+namespace Epam.HomeWork.LabRunners.Common
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents statistics info
+    /// </summary>
+    /// <typeparam name="T">Data type</typeparam>
+    public class StatisticInfo<T>
+    {
+        /// <summary>
+        /// Gets or sets name of the statistics
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets statistic data
+        /// </summary>
+        public IEnumerable<T> Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets name of the data item
+        /// </summary>
+        public string DataItemName { get; set; }
+    }
+}
---
+++ HomeWork.LabRunners/Common/SyncfusionExcelColumnReader.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="SyncfusionExcelColumnReader.cs" company="Roman Moravskyi">
+// Copyright (c) Roman Moravskyi. All rights reserved.
+// </copyright>
+
+namespace Epam.HomeWork.LabRunners.Common
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using Syncfusion.XlsIO;
+
+    /// <summary>
+    /// Used to read a range from concrete column
+    /// </summary>
+    public class SyncfusionExcelColumnReader : IExcelColumnReader
+    {
+        /// <summary>
+        /// Gets a range data from specific column
+        /// </summary>
+        /// <param name="columnName">Name of the column</param>
+        /// <param name="startRange">Start index</param>
+        /// <param name="inputFileName">File to read from</param>
+        /// <returns>List of data</returns>
+        public IEnumerable<string> GetData(string columnName, int startRange, string inputFileName)
+        {
+            List<string> elements = null;
+
+            using (FileStream inputStream = new FileStream(inputFileName, FileMode.Open))
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IWorkbook workbook = excelEngine.Excel.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                elements = new List<string>();
+                int i = startRange;
+                string data = null;
+
+                do
+                {
+                    data = worksheet.Range[$"{columnName}{i}"].Value;
+
+                    if (!string.IsNullOrWhiteSpace(data))
+                    {
+                        elements.Add(data);
+                    }
+                    ++i;
+                }
+                while (!string.IsNullOrWhiteSpace(data));
+            }
+
+            return elements;
+        }
+    }
+}
