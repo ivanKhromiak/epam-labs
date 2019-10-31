7971cade7f075295527dbc100f106e6a97406bbb
Roman Moravskyi <moravsky12@gmail.com>
2019-10-31 12:40:11 +0200

    added custom exception

---
+++ HomeWork.Common/LabConfigurationException.cs
@@ -0,0 +1,15 @@
+﻿namespace Epam.HomeWork.Lab7Runner
+{
+    using System;
+
+    [Serializable]
+    public class LabConfigurationException : Exception
+    {
+        public LabConfigurationException() { }
+        public LabConfigurationException(string message) : base(message) { }
+        public LabConfigurationException(string message, Exception inner) : base(message, inner) { }
+        protected LabConfigurationException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
