d2bd39f45ad2991af41fd4b1aa0f3af8a631eb82
Roman Moravskyi <moravsky12@gmail.com>
2019-11-14 22:27:27 +0200

    moved ContainerBuilder to separate file

---
+++ HomeWork.IoC/Container.ContainerBuilder.cs
@@ -0,0 +1,38 @@
+﻿namespace HomeWork.IoC
+{
+    using System;
+
+    public partial class Container
+    {
+        public class ContainerBuilder : IContainerBuilder
+        {
+            private readonly Type sourceType;
+            private readonly Container container;
+
+            public ContainerBuilder(Type sourceType, Container container)
+            {
+                this.sourceType = sourceType;
+                this.container = container;
+            }
+
+            public IContainerBuilder Use<TDest>(LifeTime lifeTime)
+            {
+                return this.Use(typeof(TDest), lifeTime);
+            }
+
+            public IContainerBuilder Use(Type destType, LifeTime lifeTime)
+            {
+                var newDescriptor = new ServiceDescriptor
+                {
+                    ServiceLifeTime = lifeTime,
+                    ServiceType = destType,
+                    ServiceImplementation = null
+                };
+
+                this.container.servicesMap.AddOrUpdate(this.sourceType, newDescriptor, (src, descriptor) => newDescriptor);
+
+                return this;
+            }
+        }
+    }
+}
--- HomeWork.IoC/Container.cs
+++ HomeWork.IoC/Container.cs
@@ -6,7 +6,7 @@
     using System.Reflection;
     using HomeWork.IoC.Exceptions;
 
-    public class Container : IContainer
+    public partial class Container : IContainer
     {
         private readonly ConcurrentDictionary<Type, ServiceDescriptor> servicesMap;
         private readonly ConcurrentDictionary<Type, object> genericServicesMap;
@@ -29,7 +29,7 @@
 
         public TDest Resolve<TDest>()
         {
-            return (TDest)Resolve(typeof(TDest));
+            return (TDest)this.Resolve(typeof(TDest));
         }
 
         public object Resolve(Type sourceType)
@@ -38,19 +38,19 @@
 
             if (!sourceType.IsAbstract)
             {
-                instance = CreateInstance(sourceType);
+                instance = this.CreateInstance(sourceType);
             }
-            else if (servicesMap.ContainsKey(sourceType))
+            else if (this.servicesMap.ContainsKey(sourceType))
             {
-                instance = CreateInstance(servicesMap[sourceType]);
+                instance = this.CreateInstance(this.servicesMap[sourceType]);
             }
-            else if (sourceType.IsGenericType && servicesMap.ContainsKey(sourceType.GetGenericTypeDefinition()))
+            else if (sourceType.IsGenericType && this.servicesMap.ContainsKey(sourceType.GetGenericTypeDefinition()))
             {
-                instance = CreateGenericInstance(sourceType);
+                instance = this.CreateGenericInstance(sourceType);
             }
             else
             {
-                throw new NotResolvedException($"Cannot resolve for type: {sourceType.FullName}");
+                throw new NotResolvedException($"Cannot resolve type: {sourceType.FullName}");
             }
 
             return instance;
@@ -59,21 +59,21 @@
         private object CreateGenericInstance(Type sourceType)
         {
             object instance = null;
-            var destTypeDesc = servicesMap[sourceType.GetGenericTypeDefinition()];
+            var destTypeDesc = this.servicesMap[sourceType.GetGenericTypeDefinition()];
             var closedDestType = destTypeDesc.ServiceType.MakeGenericType(sourceType.GetGenericArguments());
 
             if (destTypeDesc.ServiceLifeTime == LifeTime.Transient)
             {
-                instance = CreateInstance(closedDestType);
+                instance = this.CreateInstance(closedDestType);
             }
             else if (destTypeDesc.ServiceLifeTime == LifeTime.Singleton)
             {
-                if (!genericServicesMap.ContainsKey(closedDestType))
+                if (!this.genericServicesMap.ContainsKey(closedDestType))
                 {
-                    genericServicesMap.TryAdd(closedDestType, CreateInstance(closedDestType));
+                    this.genericServicesMap.TryAdd(closedDestType, this.CreateInstance(closedDestType));
                 }
 
-                instance = genericServicesMap[closedDestType];
+                instance = this.genericServicesMap[closedDestType];
             }
 
             return instance;
@@ -85,13 +85,13 @@
 
             if (serviceDescriptor.ServiceLifeTime == LifeTime.Transient)
             {
-                return CreateInstance(serviceDescriptor.ServiceType);
+                return this.CreateInstance(serviceDescriptor.ServiceType);
             }
             else if (serviceDescriptor.ServiceLifeTime == LifeTime.Singleton)
             {
                 if (serviceDescriptor.ServiceImplementation == null)
                 {
-                    serviceDescriptor.ServiceImplementation = CreateInstance(serviceDescriptor.ServiceType);
+                    serviceDescriptor.ServiceImplementation = this.CreateInstance(serviceDescriptor.ServiceType);
                 }
 
                 service = serviceDescriptor.ServiceImplementation;
@@ -113,41 +113,10 @@
             }
 
             var ctorParams = ctor.GetParameters()
-                                 .Select(p => Resolve(p.ParameterType))
+                                 .Select(p => this.Resolve(p.ParameterType))
                                  .ToArray();
 
             return Activator.CreateInstance(serviceType, ctorParams);
-        }
-
-        public class ContainerBuilder : IContainerBuilder
-        {
-            private readonly Type sourceType;
-            private readonly Container container;
-
-            public ContainerBuilder(Type sourceType, Container container)
-            {
-                this.sourceType = sourceType;
-                this.container = container;
-            }
-
-            public IContainerBuilder Use<TDest>(LifeTime lifeTime)
-            {
-                return this.Use(typeof(TDest), lifeTime);
-            }
-
-            public IContainerBuilder Use(Type destType, LifeTime lifeTime)
-            {
-                var newDescriptor = new ServiceDescriptor
-                {
-                    ServiceLifeTime = lifeTime,
-                    ServiceType = destType,
-                    ServiceImplementation = null
-                };
-
-                this.container.servicesMap.AddOrUpdate(this.sourceType, newDescriptor, (src, descriptor) => newDescriptor);
-
-                return this;
-            }
         }
     }
 }
